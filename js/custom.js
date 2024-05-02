29d5e8fd24b2260d218990df9eb4519a87e70327
surahio29 <absurahio@gmail.com>
2024-05-03 03:36:56 +0500

    updated website

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,42 @@
+// Get Current Year
+function getCurrentYear() {
+    var d = new Date();
+    var year = d.getFullYear();
+    document.querySelector("#displayDateYear").innerText = year;
+}
+getCurrentYear()
 
-  (function ($) {
-  
-  "use strict";
+//client section owl carousel
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: [
+        '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>'
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
-    });
+/** google_map js **/
 
-    // NAVBAR
-    $(".navbar").headroom();
-
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-
-    $('.slick-slideshow').slick({
-      autoplay: true,
-      infinite: true,
-      arrows: false,
-      fade: true,
-      dots: true,
-    });
-
-    $('.slick-testimonial').slick({
-      arrows: false,
-      dots: true,
-    });
-    
-  })(window.jQuery);
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
